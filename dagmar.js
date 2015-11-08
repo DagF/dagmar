483a709d21dcfbf1611857b65136ddbb5fb0d78d
Dag Frode Solberg <tncditto@gmal.com>
2015-11-08 18:25:47 +0100

    Implemented ul

--- dagmar.js
+++ dagmar.js
@@ -9,22 +9,48 @@ var dagmar = function (){
             else break;
         }
         if( h > 6 ) throw "Header out of range";
-        return "<h" + h + ">" + line.substring(h) + "</h" +  h + ">";
+        return "<h" + h + ">" + line.substring(h).trim() + "</h" +  h + ">";
+    }
+
+    function parseUnoderedList(list){
+        var ret = "<ul>\n";
+        for( var i = 0; i < list.length; i++){
+            ret += "<li>" + list[i].substring(1).trim() + "</li>\n";
+        }
+        ret += "</ul>\n";
+        return ret;
     }
 
     function parse(string){
+        string = string.trim();
         var output = "";
         var lines = string.split("\n");
         for(var l = 0; l < lines.length; l++){
-            var line = lines[l];
+            var line = lines[l].trim();
             if(line.charAt(0) == "#"){
                 output += parseHeader( line );
             }
-            else{
-                output += line;
+            else if(line.charAt(0) == "*"){
+                var ul = [];
+                if( lines.length > 1){
+                    while(l < lines.length && lines[l].trim().charAt(0) == "*"){
+                        ul.push(lines[l].trim());
+                        if( l < lines.length) l++;
+                    }
+                    l--;
+                    output += parseUnoderedList(ul);
+                }
+                else{
+                    output += parseUnoderedList(lines);
+                }
+
             }
+            else{
+                output += "<p>" + line + "</p>\n";
+            }
+            output += "<br>\n";
         }
-        return output;
+        return output.trim();
     }
 
     return {
--- tests.js
+++ tests.js
@@ -21,7 +21,7 @@ function assert(name,msg,exspected,functon,input){
 assert(
     "Parse empty string",
     "Empty strings should be returned",
-    "",
+    "<p></p>\n<br>",
     dagmar.parse,
     ""
 );
@@ -29,7 +29,7 @@ assert(
 assert(
     "Parse H1",
     "# shuld be parsed to H1",
-    "<h1>Test</h1>",
+    "<h1>Test</h1><br>",
     dagmar.parse,
     "#Test"
 );
@@ -37,7 +37,7 @@ assert(
 assert(
     "Parse H2",
     "## shuld be parsed to H2",
-    "<h2>Test</h2>",
+    "<h2>Test</h2><br>",
     dagmar.parse,
     "##Test"
 );
@@ -45,7 +45,7 @@ assert(
 assert(
     "Parse H3",
     "## shuld be parsed to H3",
-    "<h3>Test</h3>",
+    "<h3>Test</h3><br>",
     dagmar.parse,
     "###Test"
 );
@@ -53,7 +53,7 @@ assert(
 assert(
     "Parse H4",
     "#### shuld be parsed to H4",
-    "<h4>Test</h4>",
+    "<h4>Test</h4><br>",
     dagmar.parse,
     "####Test"
 );
@@ -61,7 +61,7 @@ assert(
 assert(
     "Parse H5",
     "##### shuld be parsed to H5",
-    "<h5>Test</h5>",
+    "<h5>Test</h5><br>",
     dagmar.parse,
     "#####Test"
 );
@@ -69,7 +69,7 @@ assert(
 assert(
     "Parse H6",
     "###### shuld be parsed to H6",
-    "<h6>Test</h6>",
+    "<h6>Test</h6><br>",
     dagmar.parse,
     "######Test"
 );
@@ -82,3 +82,51 @@ assert(
     "#######Test"
 );
 
+assert(
+    "Parse multiple lines with headers",
+    "note sure",
+    "<h1>Test</h1><br>\n<h2>Test2</h2><br>",
+    dagmar.parse,
+    "#Test\n##Test2"
+);
+
+assert(
+    "Parse multiple lines with headers and whitespace",
+    "note sure",
+    "<h1>Test</h1><br>\n<h2>Test2</h2><br>",
+    dagmar.parse,
+    "\n\n\t\n#Test\n##Test2"
+);
+
+assert(
+    "Parse ul with one element",
+    "* should be paresed to unordered list",
+    "<ul>\n<li>Test</li>\n</ul>\n<br>",
+    dagmar.parse,
+    "*Test"
+);
+
+assert(
+    "Parse ul with two elements",
+    "* should be paresed to unordered list",
+    "<ul>\n<li>Test</li>\n<li>Test2</li>\n</ul>\n<br>",
+    dagmar.parse,
+    "*Test\n*Test2"
+);
+
+assert(
+    "Parse ul with many elements",
+    "* should be paresed to unordered list",
+    "<ul>\n<li>Test</li>\n<li>Test2</li>\n<li>Test3</li>\n<li>Test4</li>\n</ul>\n<br>",
+    dagmar.parse,
+    "*Test\n*Test2\n*Test3\n*Test4"
+);
+
+
+assert(
+    "Parse ul with many elements folowed by paragraph",
+    "* should be paresed to unordered list",
+    "<ul>\n<li>Test</li>\n<li>Test2</li>\n<li>Test3</li>\n<li>Test4</li>\n</ul>\n<br>\n<p>Para</p>\n<br>",
+    dagmar.parse,
+    "*Test\n*Test2\n*Test3\n*Test4\nPara"
+);
